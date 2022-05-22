61368d6d286a511799ade0f9f4c5bdf3581a6ae6
detu <eka.suwartadi@gmail.com>
2022-05-22 13:05:00 +0200

    plan for Penanggalan class.

---
+++ Penanggalan.h
@@ -0,0 +1,13 @@
+//
+// Created by detu on 5/22/22.
+//
+
+#ifndef OTONAN_PENANGGALAN_H
+#define OTONAN_PENANGGALAN_H
+
+// see the Java class
+class Penanggalan{
+
+};
+
+#endif //OTONAN_PENANGGALAN_H
